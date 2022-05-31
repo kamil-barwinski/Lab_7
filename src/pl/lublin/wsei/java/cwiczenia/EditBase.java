b9f8604134a9bd9a7715d9df38f5e090b7fb41d3
kamil_barwinski <kamil.barwinski1bt1@gmail.com>
2022-05-31 13:08:29 +0200

    Punkt 7

---
+++ src/pl/lublin/wsei/java/cwiczenia/EditBase.java
@@ -0,0 +1,32 @@
+package pl.lublin.wsei.java.cwiczenia;
+import pl.lublin.wsei.java.cwiczenia.MyDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+import java.util.Properties;
+
+import com.mysql.jdbc.Driver;
+
+public class EditBase {
+    public static void main(String[] args) throws SQLException {
+        MyDB db = new MyDB("localhost","mydb", 3306);
+        db.setUser("root");
+        db.setPassword("root");
+        Connection conn = db.getConnection();
+        if (conn != null) {
+            Statement polecenie = conn.createStatement();
+            Number dostawa = 100;
+            polecenie.executeUpdate("UPDATE coffee_houses SET COFFEE = COFFEE + " + dostawa + " WHERE CITY = 'SF' " );
+
+            ResultSet res = polecenie.executeQuery("SELECT * FROM coffee_houses");
+            System.out.printf("%10s%20s%7s%7s%7s\n","STORE_ID","CITY","COFFE","MERCH","TOTAL");
+            while (res.next()) {
+                System.out.printf("%10s%20s%7s%7s%7s\n", res.getInt(1), res.getString("CITY"),
+                        res.getInt(3), res.getInt(4), res.getInt(5));
+            }
+        }
+        db.closeConnection();
+        System.out.println("Połączenie z bazą zakończone");
+    }
+}
